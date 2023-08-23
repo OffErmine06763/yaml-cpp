f572b3ec9f4d9bb673e48a139384e2b02755360c
Alessandro Boschi <67075812+OffErmine06763@users.noreply.github.com>
2023-08-23 21:59:07 +0200

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,33 @@
+project "Yaml"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+    "src/**.cpp",
+
+    "include/**.h"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++20"
+		staticruntime "on"
+
+  filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++20"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
